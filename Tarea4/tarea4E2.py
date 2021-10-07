5b1cac5f88ac7552ddf11947f3be380c2ea480d8
Pablo Jimenez <jup_jim_7@hotmail.com>
2021-10-06 19:20:17 -0500

    Correxiones

--- Tarea4/tarea4E2.py
+++ Tarea4/tarea4E2.py
@@ -2,14 +2,17 @@ primerNumero= int(input("Ingrese el primer número:\n"))
 segundoNumero= int(input("Ingrese el segundo número:\n"))
 tercerNumero= int(input("Ingrese el tercer número:\n"))
 
-if (primerNumero > segundoNumero and segundoNumero > tercerNumero):
-    print(f'El primer número ({primerNumero}) es mayor que el segundo número ({segundoNumero}) y el tercer número({tercerNumero}))')
-
-elif(segundoNumero> primerNumero and primerNumero> tercerNumero):
-     print(f'El segundo número ({segundoNumero}) es mayor que el primer número ({primerNumero}) y el tercer número({tercerNumero}))')
-
+if(primerNumero < segundoNumero and tercerNumero > primerNumero):
+    print(f"{primerNumero} es el menor")
+elif(segundoNumero<primerNumero and tercerNumero > segundoNumero):
+    print(f"{segundoNumero} es el menor")
+elif(primerNumero ==segundoNumero and primerNumero<tercerNumero):
+    print(f'{primerNumero} y {segundoNumero} son menores')
+elif(segundoNumero == tercerNumero and segundoNumero<primerNumero):
+    print(f'{segundoNumero} y {tercerNumero} son menores')
+elif(primerNumero == tercerNumero and segundoNumero> primerNumero):
+    print(f'{primerNumero} y {tercerNumero} son menores')
 elif(primerNumero == segundoNumero and primerNumero == tercerNumero):
-    print('Los tres números son iguales')
-
+    print("Los números son iguales")
 else:
-     print(f'El tercer número ({tercerNumero}) es mayor que el primer número ({primerNumero}) y el segundo número({segundoNumero}))')
+    print(f'{tercerNumero} es el menor')
